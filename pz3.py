daee5b0df01fe405e38e88ed2aecde3a37df53e3
m1dcrab <koctabarm@gmail.com>
2024-09-20 00:16:36 +0300

    Delete pz3.py

--- pz3.py
+++
@@ -1,5 +0,0 @@
-homeworkDone = 12
-timeSpent = 1.5
-courseName = 'Python'
-timeOnOneTask = timeSpent/homeworkDone
-print ('Курс:',courseName+',','всего задач:',str(homeworkDone)+',','затрачено часов:',str(timeSpent)+',','среднее время выполнения:',timeOnOneTask,'часа.')
