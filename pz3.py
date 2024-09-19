48567c4f540b3c8cf1858c2e702429314130c957
m1dcrab <koctabarm@gmail.com>
2024-09-19 21:05:26 +0300

    pz3.py

--- pz3.py
+++ pz3.py
@@ -2,4 +2,4 @@ homeworkDone = 12
 timeSpent = 1.5
 courseName = 'Python'
 timeOnOneTask = timeSpent/homeworkDone
-print ('Курс:'+courseName,'всего задач:',homeworkDone,'затрачено часов:',timeSpent,'среднее время выполнения:',timeOnOneTask,'часа')
+print ('Курс:',courseName,'всего задач:',homeworkDone,'затрачено часов:',timeSpent,'среднее время выполнения:',timeOnOneTask,'часа')
