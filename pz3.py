9c24f0f604629b9ea16ff66f06ddf9cf6618ecee
m1dcrab <koctabarm@gmail.com>
2024-09-19 20:58:49 +0300

    Add files via upload

---
+++ pz3.py
@@ -0,0 +1,5 @@
+homeworkDone = 12
+timeSpent = 1.5
+courseName = 'Python'
+timeOnOneTask = timeSpent/homeworkDone
+print ('Курс:'+courseName,'всего задач:',homeworkDone,'затрачено часов:',timeSpent,'среднее время выполнения:',timeOnOneTask,'часа')
