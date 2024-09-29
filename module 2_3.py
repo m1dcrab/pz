152bfd84456551725c3ec808b0ed2ba9551198da
m1dcrab <koctabarm@gmail.com>
2024-09-29 13:41:23 +0300

    Update module 2_3.py

--- module 2_3.py
+++ module 2_3.py
@@ -1,10 +1,11 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-count = -1
-while True:
-    count = count + 1
+count = 0
+while len(my_list) > count:
     if my_list[count] > 0:
         print(my_list[count])
+        count = count + 1
     elif my_list[count] == 0:
+        count = count + 1
         continue
-    else:
+    elif my_list[count] < 0:
         break
