77f9aecf4bac98e3cf7934a86e01839978835615
m1dcrab <koctabarm@gmail.com>
2024-11-27 04:49:26 +0300

    Update module_9_2.py

--- module_9/module_9_2.py
+++ module_9/module_9_2.py
@@ -1,9 +1,9 @@
 first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
 second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
 
-first_result = [len(i) for i in first_strings if len(i) >= 5]
-second_result = [(f,s) for f in first_strings for s in second_strings if len(f)==len(s)]
-third_result = {t:len(t) for t in first_strings + second_strings if len(t) % 2 == 0}
+first_result = [len(f_str) for f_str in first_strings if len(f_str) >= 5]
+second_result = [(f_str, s_str) for f_str in first_strings for s_str in second_strings if len(f_str) == len(s_str)]
+third_result = {t_str:len(t_str) for t_str in first_strings + second_strings if len(t_str) % 2 == 0}
 
 print(first_result)
 print(second_result)
